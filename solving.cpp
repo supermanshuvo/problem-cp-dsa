1f2769930eaab4bed290c14482bfe9ff88607db6
supermanshuvo <shuvothesuperman@gmail.com>
2021-12-06 01:35:05 +0600

    test

--- solving.cpp
+++ solving.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
-    cout<< "Set up done"<< endl;
+    long long t,i,n;
+    cin >> t;
+    for( i=1;i<=t;i++){
+        cin >>n;
+        long int row = (sqrt(1+4*2*n)-1)/2;
+        cout << row << endl;
+    }
     return 0;
 }
