d2c8ea1cd72e34e2d766ce7011b5245ac3202410
supermanshuvo <shuvothesuperman@gmail.com>
2021-12-07 23:55:34 +0600

    Problem solv done

--- solving.cpp
+++ solving.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 int main()
 {
-    cout<< "Welcome to the world" <<endl;
+    int t;
+    cin >> t;
+    for(int i=1;i<=t;i++){
+        int n;
+        cin >> n;
+        long int result = (((((n*567)/9)+7492)*235)/47)-498;
+        int output = abs((result%100)/10);
+        cout<< output <<endl;
+    }
     return 0;
 }
