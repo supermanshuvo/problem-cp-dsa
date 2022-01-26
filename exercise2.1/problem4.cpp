7337f596f4a9e81b75ededdff3e301727d22b3c8
supermanshuvo <shuvothesuperman@gmail.com>
2022-01-26 11:49:26 +0600

    Problem 4 in book is solve

---
+++ exercise2.1/problem4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int r;
+    cin >> r;
+    float c = 2*M_PI*r;
+    cout << fixed<< setprecision(2) << c << endl;
+    return 0;
+}
