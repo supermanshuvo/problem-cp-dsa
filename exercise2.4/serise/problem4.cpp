412702b27ef637ddbec7234ebf19d10e7b34ac52
supermanshuvo <shuvothesuperman@gmail.com>
2022-01-28 23:55:23 +0600

    Problem set 2.4 n term problem number 04 is solve

---
+++ exercise2.4/serise/problem4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n, sum = 0, num = 1;
+    cin >> n;
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= i; j++){
+            sum+=num++;
+        }
+    }
+    cout << sum << endl;
+    return 0;
+}
