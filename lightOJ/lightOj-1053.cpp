d7781243b474ad197ada616b61d99da51b60c975
supermanshuvo <shuvothesuperman@gmail.com>
2022-02-01 19:49:03 +0600

    Light oj problem number 1053 is done

---
+++ lightOJ/lightOj-1053.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file lightOj-1053.cpp
+ * @author supermanshuvo (shuvothesuperman@gmail.com)
+ * @brief Higher Math
+ * 
+ * @version 0.1
+ * @date 2022-02-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t,arr[3];
+    cin >> t;
+    for (int i = 1; i <= t; i++){
+        cin >> arr[0] >> arr[1] >> arr[2];
+        sort(arr, arr + 3);
+        int hypo = arr[2] * arr[2];
+        int other = arr[0] * arr[0] + arr[1] * arr[1];
+        cout << "Case " << i << ": " << (hypo == other ? "yes" : "no") << endl;
+    }
+    return 0;
+}
