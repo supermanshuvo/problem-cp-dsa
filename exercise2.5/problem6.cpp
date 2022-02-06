ec9ccfb9c78c2faff07c24faedbb9bdff2947a37
supermanshuvo <shuvothesuperman@gmail.com>
2022-02-06 11:52:04 +0600

    new file create for 1 and 0 problem

---
+++ exercise2.5/problem6.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file problem6.cpp
+ * @author supermanshuvo (shuvothesuperman@gmail.com)
+ * @brief string length with libary function and with out;
+ * @version 0.1
+ * @date 2022-02-06
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
