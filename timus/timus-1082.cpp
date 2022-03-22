637e41a0e754300d07cb0e482f4cf0021049ba47
supermanshuvo <shuvothesuperman@gmail.com>
2022-03-22 11:05:36 +0600

    timus-1082 problem is soleve

--- timus/timus-1082.cpp
+++ timus/timus-1082.cpp
@@ -8,14 +8,17 @@
  * @copyright Copyright (c) 2022
  * 
  */
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main(){
-     int n;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        printf("%d ", i);
-    }
-    putchar('\n');
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    for(int i = 1; i <= n; ++i)
+        cout << i << ' ';
+
     return 0;
 }
